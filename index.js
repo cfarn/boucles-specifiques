30692adcbaf027a647c8cacb72b7557c3c4ceb49
cfarn <58476975+cfarn@users.noreply.github.com>
2022-06-28 17:02:22 +0200

    ex 6

--- index.js
+++ index.js
@@ -18,42 +18,104 @@ const log = (console.log)
 
 // 02 - Map Names
 
-let longNames = 
-[
-	{
-		firstName: "Jane",
-		lastName: "Doe"
-	},
-	{
-		firstName: "John",
-		lastName: "Smith"
-	}
-]
+// let longNames = 
+// [
+// 	{
+// 		firstName: "Jane",
+// 		lastName: "Doe"
+// 	},
+// 	{
+// 		firstName: "John",
+// 		lastName: "Smith"
+// 	}
+// ]
 // log(Object.values(longNames))
 
-let shortNames = longNames.map((longName) => {
-    return {
-        name: Object.values(longName)
+// let shortNames = longNames.map((longName) => {
+//     return {
+//         name: Object.values(longName)
         
-    }
-})
-log(shortNames)
+//     }
+// })
+// log(shortNames)
 
 // 03 - Filter Numbers
 
 // let array = [1, "toto", 34, "javascript", 8]
 // log(array)
-// log(Object.keys(array))
 
-// let numbers = array.filter(number => typeof number ==="number") 
+// let numbers = array.filter(number => typeof number === "number") 
 // log(numbers)
 
 // 04 - Filter Even
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
-let even = numbers.filter((number) => {
-    if(number % 2 === 0) {
+// let numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+// let even = numbers.filter((number) => {
+//     if(number % 2 === 0) {
+//         return true
+//     }
+// })
+// log(even)
+
+// 05 - Cakes
+
+let cakes = 
+[
+	{
+		name: "cake",
+		flavor: "vanilla",
+		status: "available"
+	},
+	{
+		name: "brownie",
+		flavor: "chocolate",
+		status: "available"
+	},
+	{
+		name: "pie",
+		flavor: "strawberry",
+		status: "available"
+	},
+	{
+		name: "muffin",
+		flavor: "pistachio",
+		status: "available"
+	},
+	{
+		name: "donut",
+		flavor: "chocolate",
+		status: "available"
+	},
+]
+
+let chocoCakes = cakes.filter((cake) => {
+    if(cake.flavor === "chocolate") {
         return true
     }
 })
-log(even)
+log(chocoCakes)
+
+let chocoCakesStatus = chocoCakes.map((chocoCake) => {
+    // return {
+    //     status: "sold out !"
+    // }
+    // if(chocoCake.status === "available") {
+    //     return "sold out !"
+    // }
+})
+log(chocoCakesStatus)
+
+// chocoCakes = chocoCakesStatus.map((chocoCake) => {
+//     return {
+//         Object.entries(a)
+        
+//     }
+// })
+// log(chocoCakes)
+
+// 06 - Cake v2
+
+let pie = cakes.find((cake) => {
+    return cake.name === "pie"
+})
+log(pie)
