15a4f2a9d37e6f3532e93006ae4f323578c239ec
cfarn <58476975+cfarn@users.noreply.github.com>
2022-06-28 15:04:39 +0200

    ex 2-3

---
+++ index.js
@@ -0,0 +1,52 @@
+const log = (console.log)
+
+// 01 - Map Double
+
+// let array = [1, 2, 3, 4, 5]
+// let double = array.map(() => {
+//     return array
+// })
+// log(double)
+
+// let numbers = [1, 2, 3, 4, 5]
+// let double = numbers.map((number) => {
+//     return number
+// })
+// log(double)
+
+
+
+// 02 - Map Names
+
+let longNames = 
+[
+	{
+		firstName: "Jane",
+		lastName: "Doe"
+	},
+	{
+		firstName: "John",
+		lastName: "Smith"
+	}
+]
+// log(Object.values(longNames))
+
+let shortNames = longNames.map((longName) => {
+    return {
+        name: Object.values(longName)
+        
+    }
+})
+log(shortNames)
+
+// 03 - Filter Numbers
+
+let array = [1, "toto", 34, "javascript", 8]
+log(array)
+log(Object.keys(array))
+
+let numbers = array.filter(number => typeof number ==="number") 
+log(numbers)
+
+// 04 - Filter Even
+
